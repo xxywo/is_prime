3b361b6306a811545c3ef5306e8f1192f458e0a7
labushi de <58633616+xxywo@users.noreply.github.com>
2021-06-21 05:43:47 +0000

    timer

--- main.cs
+++ main.cs
@@ -1,12 +1,18 @@
 using System;
+//version 1, might need to optimize
+//set timer to see the performancn
+
+
 
 class MainClass {
   static bool isPrime(int n){
-    if(n <= 1){
+    if(n <= 1){//when n is negative or 1
       return false;
     }
      
     for(int i = 2; i < n; i++){
+      //a prime number only divisible by 1 and itself
+      //if n is divisible to any number other than 1 and itself, it's not a prime number
       if(n%i ==0){
         return false;
       }
@@ -14,14 +20,20 @@ class MainClass {
     
     return true;
   }
-
+  //main funtion 
   public static void Main (string[] args) {
+    //test case
+    var watch = new System.Diagnostics.Stopwatch();
+    watch.Start();
      for(int i = 0; i < 100; i++){
        if(isPrime(i)){
         Console.WriteLine("{0} is prime", i);
       }
 
      }
+     watch.Stop();
+     Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
      
       
     }
