8fa64fac8566055c9af02fa8b631241f58f675ba
labushi de <58633616+xxywo@users.noreply.github.com>
2021-06-21 05:47:42 +0000

    using_recusive

--- main.cs
+++ main.cs
@@ -2,14 +2,15 @@ using System;
 //version 1, might need to optimize
 //set timer to see the performancn
 
+  
 
 
 class MainClass {
+  static int i = 2;
   static bool isPrime(int n){
     if(n <= 1){//when n is negative or 1
       return false;
     }
-     
     for(int i = 2; i < n; i++){
       //a prime number only divisible by 1 and itself
       //if n is divisible to any number other than 1 and itself, it's not a prime number
@@ -20,13 +21,32 @@ class MainClass {
     
     return true;
   }
+  static bool is_Prime(int n)
+    {
+      
+        // corner cases
+        if (n == 0 || n == 1) {
+            return false;
+        }
+      
+        // Checking Prime
+        if (n == i)
+            return true;
+      
+        // base cases
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+        return isPrime(n);
+    }
   //main funtion 
   public static void Main (string[] args) {
     //test case
     var watch = new System.Diagnostics.Stopwatch();
     watch.Start();
      for(int i = 0; i < 100; i++){
-       if(isPrime(i)){
+       if(is_Prime(i)){
         Console.WriteLine("{0} is prime", i);
       }
 
