c798a5e38bc4eba2cbf9550342377fd1da3dc98f
labushi de <58633616+xxywo@users.noreply.github.com>
2021-06-21 05:04:17 +0000

    n

--- main.cs
+++ main.cs
@@ -2,6 +2,12 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    int input = 4;
+    for(int i = 0; i < input; i++){
+      if(input%2 == 0){
+        Console.Write("false");
+      }
+      
+    }
   }
 }
